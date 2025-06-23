678798736b24ae0d1f0eec196d996198a2c3d84c
Chris <chris@clyates.plus.com>
2025-06-23 20:55:56 +0100

    Add panel and tab layouts

---
+++ resources/layouts/panel.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @var string $content
+ * @var string $title
+ */
+?>
+
+<h1><?= $title ?></h1>
+<div class="tracy-inner">
+    <div class="tracy-inner-container">
+        <?= $content ?>
+    </div>
+</div>
---
+++ resources/layouts/tab.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @var string $content
+ * @var string $icon
+ * @var string $title
+ */
+?>
+<span title="<?= $title ?>">
+    <?= $icon ?>
+    <span class="tracy-label">
+        <?= $content ?>
+    </span>
+</span>
