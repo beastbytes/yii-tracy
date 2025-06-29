283f50b4d1098e93c8a976a5ccfacd18f2e9bb76
Chris <chris@clyates.plus.com>
2025-06-29 16:37:03 +0100

    Update events config

--- config/events-web.php
+++ config/events-web.php
@@ -3,10 +3,8 @@
 declare(strict_types=1);
 
 use BeastBytes\Yii\Tracy\Tracy;
-use Yiisoft\View\Event\WebView\AfterRender;
 use Yiisoft\Yii\Http\Event\AfterEmit;
 use Yiisoft\Yii\Http\Event\ApplicationStartup;
-use Yiisoft\Yii\View\Renderer\Debug\WebViewCollector;
 
 return [
     ApplicationStartup::class => [
@@ -14,8 +12,5 @@ return [
     ],
     AfterEmit::class => [
         [Tracy::class, 'stop'],
-    ],
-    AfterRender::class => [
-        [WebViewCollector::class, 'collect'],
     ],
 ];
